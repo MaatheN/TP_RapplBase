c10e38759a693a8012a1f7d1629f063411fda8d2
MaatheN <mathieu.neveu@viacesi.fr>
2019-01-08 10:03:17 +0100

    Ca marche 1.0

--- index.php
+++ index.php
@@ -17,17 +17,12 @@ $contacts= [
         '14'
         ]
 ];
-//foreach ($contacts['age'] as $ages){
-    //if($ages>='18'){
-        //echo "majeur \n";
-    //};
-if ($contacts['age'][0] == 23){
-    echo "ça marche!";
-}
 while($index_val < 4){
     if($contacts['age'][$index_val] >=18){
-        echo $contacts['nom'][$index_val].$contacts['prenom'][$index_val].$contacts['age'][$index_val];
+        echo $contacts['nom'][$index_val]." "
+            .$contacts['prenom'][$index_val]." "
+            .$contacts['age'][$index_val]." "
+            ."\n";
     }
-
     $index_val++;
 }
