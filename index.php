d9d2ae6b7476ec480de444ad5c22471442c004fd
MaatheN <mathieu.neveu@viacesi.fr>
2019-01-08 10:12:04 +0100

    Ca marche 1.3

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 $index_val = 0;
+$nb_contacts = 4;
 $contacts= [
     'nom' => ['Jean',
         'Marie',
@@ -17,8 +18,11 @@ $contacts= [
         '14'
         ]
 ];
-while($index_val < 4){
+//boucler chaque âge de tous les contacts
+while($index_val < $nb_contacts){
+    //si un âge est > = à 18
     if($contacts['age'][$index_val] >=18){
+        //afficher le prenom de l'index du contact en question
         echo $contacts['prenom'][$index_val]."\n";
     }
     $index_val++;
