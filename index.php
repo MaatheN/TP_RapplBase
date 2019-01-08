4f8af47fa173b985ada5e48955fb18cf7174124e
MaatheN <mathieu.neveu@viacesi.fr>
2019-01-08 09:54:03 +0100

    Ca marche presque!

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+$index_val = 0;
 $contacts= [
     'nom' => ['Jean',
         'Marie',
@@ -16,8 +17,17 @@ $contacts= [
         '14'
         ]
 ];
-foreach ($contacts['age'] as $ages){
-    if($ages>='18'){
-        echo "majeur \n";
-    };
+//foreach ($contacts['age'] as $ages){
+    //if($ages>='18'){
+        //echo "majeur \n";
+    //};
+if ($contacts['age'][0] == 23){
+    echo "ça marche!";
+}
+while($index_val < 4){
+    if($contacts['age'][$index_val] >=18){
+        echo $contacts['nom'][$index_val].$contacts['prenom'][$index_val].$contacts['age'][$index_val];
+    }
+
+    $index_val++;
 }
